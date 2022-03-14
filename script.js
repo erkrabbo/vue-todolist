6ca40a14c8b389e8f3d51f25d68dc5884cc9a7de
Luca Andaloro <luca.andaloro4@gmail.com>
2022-03-14 17:22:15 +0100

    animazione eliminazione elemento lista

--- script.js
+++ script.js
@@ -4,17 +4,47 @@ const todoList = new Vue({
         newTask: '',
         taskList:[
             {
+                id: 1,
                 text: 'Fare i compiti',
                 done: false,
-            }
+            },
+            {
+                id: 2,
+                text: 'Fare i compiti',
+                done: false,
+            },
+            {
+                id: 3,
+                text: 'Fare i compiti',
+                done: false,
+            },
+            {
+                id: 4,
+                text: 'Fare i compiti',
+                done: false,
+            },
+            {
+                id: 5,
+                text: 'Fare i compiti',
+                done: false,
+            },
+            {
+                id: 6,
+                text: 'Fare i compiti',
+                done: false,
+            },
         ],
     },
     methods: {
         removeTask(taskIndex){
             this.taskList.splice(taskIndex, 1);
+            this.taskList.slice(taskIndex).forEach((element, index) => {
+                element.id = taskIndex + index + 1;
+            });
         },
         addTask(){
             const newTask = {
+                id: this.taskList.length + 1,
                 text: this.newTask.trim()[0].toUpperCase() + this.newTask.trim().slice(1).toLowerCase() ,
                 done: false,
             }
